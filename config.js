d7f38eef8360ee723e7517ea426b3a041651ad37
filipuswork <filipus351@guru.sma.belajar.id>
2026-09-22 07:54:16 +0000

    config: update URL Apps Script baru + token XI

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 /* ============================================================
-   KONFIGURASI UJIAN — TEKS BERITA 11.3
+   KONFIGURASI UJIAN — TEKS BERITA KELAS XI.3 & XI.4
    ============================================================ */
 
-window.SERVER_URL = 'https://script.google.com/macros/s/AKfycbzkUs1jSwf1ZnfeSOLzNZnZ-gQvBq66CsTW5wA0AQzNjdpeBcO_AVxzA5TbzZmZQdr3og/exec';
+window.SERVER_URL = 'https://script.google.com/macros/s/AKfycbzQVh8JEgTcvnmnykCG9zMWzwW4a4pfEEOrtCF5NPowmW6w-ptbNriefy0gqSyVXZW3LA/exec';
 
 window.DURASI_MENIT = 60;
 
-window.TOKEN_UJIAN = 'TEKS-BERITA-11.3-2025';
+window.TOKEN_UJIAN = 'TEKS-BERITA-XI-2025';
 
 window.MAX_VIOLATIONS = 3;
